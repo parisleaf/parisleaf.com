ebc0d023828df7790364b739d1f9867fd34bdea3
Andrew Clark <acdlite@me.com>
2015-01-04 14:33:03 -0600

    Fix after_success script

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Deploy to staging server if on branch `develop`
-if [ test $TRAVIS_PULL_REQUEST == "false" && test $TRAVIS_BRANCH == "develop" ]; then
+if test "$TRAVIS_PULL_REQUEST" == "false" && test "$TRAVIS_BRANCH" == "develop"; then
   # Add deploy key
   chmod 600 .travis/deploy_key.pem
   eval `ssh-agent -s`
