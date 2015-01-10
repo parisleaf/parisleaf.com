9528ee7fc0ff4a15a81df252a7f458367f8027b6
Andrew Clark <acdlite@me.com>
2015-01-10 13:10:57 -0500

    Missing space

--- src/server/routes/views/appIndex.js
+++ src/server/routes/views/appIndex.js
@@ -8,7 +8,7 @@ import prepareForRun from '../../../shared/prepareForRun';
 export default function(app) {
   app.get(/.*/, function *() {
     let { Handler, state } = yield new Promise((resolve, reject) => {
-      Router.run(routes, this.path, (Handler,state) => resolve({ Handler, state }));
+      Router.run(routes, this.path, (Handler, state) => resolve({ Handler, state }));
     });
 
     yield prepareForRun(state);
