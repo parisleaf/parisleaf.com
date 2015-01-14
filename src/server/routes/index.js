faceeb41dd3996a0f0d01e8958a6edbefe10fc15
Joseph Furlott <jmfurlott@gmail.com>
2015-01-14 16:38:57 -0500

    added /styleguide route; base component started

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,9 +3,12 @@
 import middleware from './middleware';
 import api from './api';
 import appIndex from './views/appIndex';
+import Styleguide from './views/Styleguide';
 
 export default function(app) {
   middleware(app);
   api(app);
+  Styleguide(app);
   appIndex(app);
+  
 }
