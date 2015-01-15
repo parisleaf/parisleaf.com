1b413142bd14bdd5278c43bdade9b36dfe5fc834
Andrew Clark <acdlite@me.com>
2015-01-15 14:31:24 -0500

    Remove modular-scale

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -9,8 +9,6 @@
  * By convention, these values are exported inside objects prefixed with 'sass'.
  */
 
-var ms = require('modular-scale');
-
 var colors = {
   gray: '#4d4d4d',
   yellow: '#f5d23e',
