abb0e523654bba2e979b7d3250a40e46ed5f74ac
Andrew Clark <acdlite@me.com>
2015-01-15 22:58:02 -0500

    Fail gracefully if Twitter keys not set

--- src/server/routes/api/Twitter.js
+++ src/server/routes/api/Twitter.js
@@ -1,12 +1,25 @@
 'use strict';
 
+/**
+ * Interface to the Twitter API
+ */
+
 import Twit from 'twit';
 
-/* Twitter interface to the api */
-let T = new Twit({ 
-   consumer_key: process.env.TWIT_CONSUMER_KEY,
-   consumer_secret: process.env.TWIT_CONSUMER_SECRET,
-   access_token: process.env.TWIT_ACCESS_TOKEN,
-   access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
-   }); 
+let T;
+
+if (
+  process.env.TWIT_CONSUMER_KEY
+  && process.env.TWIT_CONSUMER_SECRET
+  && process.env.TWIT_ACCESS_TOKEN
+  && process.env.TWIT_ACCESS_TOKEN_SECRET
+) {
+  T = new Twit({
+     consumer_key: process.env.TWIT_CONSUMER_KEY,
+     consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+     access_token: process.env.TWIT_ACCESS_TOKEN,
+     access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET,
+  });
+}
+
 export default T;
--- src/server/routes/api/tweets.js
+++ src/server/routes/api/tweets.js
@@ -4,6 +4,10 @@ import T from './Twitter';
 
 export default function(app) {
   app.get('/api/tweets', function *() {
+    if (typeof T === 'undefined') {
+      throw new Error('Twitter authentication error');
+    }
+
     let tweets = yield new Promise((resolve, reject) => {
       T.get('statuses/user_timeline', {count: '5'},  function (err, data, response) {
         resolve(data);
@@ -14,11 +18,16 @@ export default function(app) {
 
 
   app.get('/api/tweets/:id', function *() {
+    if (typeof T === 'undefined') {
+      throw new Error('Twitter authentication error');
+    }
+
     let tweets = yield new Promise((resolve, reject) => {
       T.get('statuses/show/:id', { id: this.params.id }, function(err, data, response) {
         resolve(data);
       });
     });
+
     this.body = tweets;
   });
 }
