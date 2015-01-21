2f2dd36eec2212b51af31f64fae7f2a146052241
Andrew Clark <acdlite@me.com>
2015-01-21 11:25:58 -0500

    Send server's initial media state to client

--- src/client/app.js
+++ src/client/app.js
@@ -4,6 +4,17 @@
 require('../shared/init');
 import './init';
 
+let initialMediaStateEl = document.getElementById('initial-media-state');
+
+if (initialMediaStateEl) {
+  let initialMediaState = JSON.parse(initialMediaStateEl.innerHTML);
+
+  import MediaMixin from 'react-media-mixin';
+  MediaMixin.getInitialState = function () {
+    return { media: initialMediaState };
+  };
+}
+
 import React from 'react';
 import Router from 'react-router';
 import routes from '../shared/routes';
