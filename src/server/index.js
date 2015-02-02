e72657192413d38f12b300d7a47640b8511a372a
Andrew Clark <acdlite@me.com>
2015-02-02 14:51:41 -0500

    Add error logging

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,13 @@ dotenv.load();
 
 import app from './app';
 
+let winston = require('winston');
+winston.add(winston.transports.File, { filename: 'parisleaf.com.log' });
+
+app.on('error', (e) => {
+  winston.error(e.message);
+});
+
 // Start listening
 app.listen(process.env.PORT);
 console.log(`App started listening on port ${process.env.PORT}`);
