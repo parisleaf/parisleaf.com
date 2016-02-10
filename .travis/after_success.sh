c48e251388a9c4531ac6c577abd7a477127cee54
Eric Leite <eric@parisleaf.com>
2016-02-10 14:17:27 -0500

    Updated travis config to only build master branch

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Deploy to staging server if on branch `develop`
+# Deploy to staging server if on branch `master`
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
   # Add deploy key
   chmod 600 .travis/deploy_key.pem
