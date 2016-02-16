83ed0ea87c43cef0ef1b646faa22e1253b90c81b
Eric Leite <eric@parisleaf.com>
2016-02-16 15:33:41 -0500

    Updated travis-ci deployment to new parisleaf server

--- .travis/after_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Deploy to production server if on branch `master`
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-  # Add deploy key
-  chmod 600 .travis/deploy_key.pem
-  eval `ssh-agent -s`
-  ssh-add .travis/deploy_key.pem
-fi
