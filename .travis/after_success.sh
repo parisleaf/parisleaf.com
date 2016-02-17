e550633261d2d5b7434b77fb01265fa7edfc039c
Eric Leite <eric@parisleaf.com>
2016-02-17 14:01:50 -0500

    Updated Travis-CI after_success script

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-chmod 600 .travis/deploy_key.pem # this key should have push access
 
-expect << EOF
-  spawn ssh-add .travis/deploy_key.pem
-  expect "Enter passphrase"
-  send "\n";
-  expect eof
-EOF
+# Add deploy key
+chmod 600 .travis/deploy_key.pem
+eval `ssh-agent -s`
+ssh-add .travis/deploy_key.pem
 
+# Add git remote
 git remote add deploy dokku@paisleaf.com:production
+
+# Push to dokku-alt server
 git push deploy
